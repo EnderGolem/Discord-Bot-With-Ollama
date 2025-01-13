0a44d9c476d1828dfba52da71ca091895cb547d0
Grisha <ashgir01@gmail.com>
2025-01-13 23:21:55 +0300

    add: message history

--- DiscordClient.cs
+++ DiscordClient.cs
@@ -1,14 +1,19 @@
 ﻿using Discord.WebSocket;
 using Discord;
-using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Linq;
 
 namespace DiscordBot;
 
 internal class DiscordClient
 {
+    private const int _maxCountOfMessagesInHistory = 7;
+
     private readonly OllamaClient _clientOllama;
     private readonly DiscordSocketClient _clientSocketDiscord;
     private readonly string _token;
+
+    private Dictionary<string, Queue<string>> historyChatsOfChannel = new(); 
 
     public DiscordClient(string token, OllamaClient ollamaClient)
     {
@@ -50,22 +55,47 @@ internal class DiscordClient
         if (arg.Author.IsBot)
             return;
 
-        Console.WriteLine($"Received: {arg.Content}");
+        Console.WriteLine($"Received: {arg.Content} from {arg.Author.Username}");
+
         if (arg.Content.Contains("/ping"))
         {
             await arg.Channel.SendMessageAsync("Pong!");
             return;
         }
 
-        _ = Task.Run(() => GenerateAndSendMessage(arg));
+        AddToHistory(arg.Channel.Name, arg.Author.Username, arg.Content);
+    
+        _ = Task.Run(() => GenerateAndSendMessage(arg.Channel, GetMessageFromHistory(arg.Channel.Name)));
     }
 
-    private async Task GenerateAndSendMessage(SocketMessage arg)
+    private async Task GenerateAndSendMessage(ISocketMessageChannel channel, string message)
     {
         //TODO вынести это в отдельную обработку, чтобы он мог продолжать слушать, даже когда генерирует ответ
-        var answer = await _clientOllama.GetResponseAsync(arg.Content);
-
+        var answer = await _clientOllama.GetResponseAsync(message);
+        AddToHistory(channel.Name, "Лами", answer);
         Console.WriteLine($"Sending message: {answer}");
-        await arg.Channel.SendMessageAsync(answer);
+        await channel.SendMessageAsync(answer);
+    }
+
+    private string GetMessageFromHistory(string channel) 
+    {
+        StringBuilder sb = new StringBuilder();
+
+        foreach(var message in historyChatsOfChannel[channel].SkipLast(1))        
+            sb.AppendLine(message);
+        sb.AppendLine($"Последнее сообщение\n{historyChatsOfChannel[channel].Last()}");
+        return sb.ToString();
+    }
+
+    private void AddToHistory(string channel, string author, string message)
+    {
+        if(historyChatsOfChannel.ContainsKey(channel))
+             historyChatsOfChannel[channel].Enqueue($"{author}: {message}");
+        else
+            historyChatsOfChannel[channel] = new Queue<string>(new[] { $"{author}: {message}" });
+
+
+        if (historyChatsOfChannel[channel].Count > _maxCountOfMessagesInHistory)        
+            historyChatsOfChannel[channel].Dequeue();       
     }
 }
